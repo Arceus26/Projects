908959499e9e55de9f72901e546c83f880018715
Arceus26 <132107141+Arceus26@users.noreply.github.com>
2024-03-16 16:27:22 +0200

    Update Lexicografic.cpp

--- C++ Permutation calculation/Lexicografic.cpp
+++ C++ Permutation calculation/Lexicografic.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-// Acest mini-proiect in C ofera o metoda pentru calcularea rangului permutarilor atat ale literelor, cat si ale cifrelor. De asemenea, ilustreaza permutarea 
+// Acest mini-proiect in C++ ofera o metoda pentru calcularea rangului permutarilor atat ale literelor, cat si ale cifrelor. De asemenea, ilustreaza permutarea 
 // anterioara, precum si urmatoarea din punct de vedere lexicografic ale unei multimi total ordonate S, verificand mai intai daca astfel de permutari exista.
 
 // Exemplu:
